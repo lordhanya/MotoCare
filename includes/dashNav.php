43138b61c518f7940c51c3247941b258123cbde3
lordhanya <ashifrahman8638471722@gmail.com>
2025-11-13 10:18:57 +0530

    profile.php completed, profile picture is not editable, but the rest are editable. logout.php added and working.  Sidebar.php and dashnav.php  went through small enhancement (style and code).

--- includes/dashNav.php
+++ includes/dashNav.php
@@ -1,20 +1,15 @@
 <!-- Dashboard Navbar -->
-<nav class="navbar navbar-expand-lg border-bottom border-body fixed-top px-5 py-3 dashNav" data-bs-theme="dark" data-aos="slide-down" data-aos-duration="1000">
+<nav class="navbar navbar-expand-lg border-body fixed-top px-5 py-3 dashNav" data-bs-theme="dark" data-aos="slide-down" data-aos-duration="1000">
     <div class="container-fluid">
         <div class="d-flex align-items-center gap-3">
             <a class="btn sidebarToggle-btn" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
                 <i class="bi bi-list"></i>
             </a>
-            <a class="navbar-brand" href="dashboard.php">Auto<span>Care</span></a>
+            <a class="navbar-brand ms-5" href="dashboard.php">Auto<span>Care</span></a>
         </div>
-        <div class="profile-welcome ms-auto text-white d-flex align-items-center me-3">
+        <div class="profile-welcome ms-auto text-white d-flex align-items-center gap-3">
             <i class="bi bi-person-circle fs-1"></i>
-            <!-- <div class="user-name text-white d-grid justify-content-center align-items-center text-left">
-                    <p>Welcome</p>
-                    
-                </div> -->
+            <a class="nav-link logout-btn me-5" href="logout.php">Logout</a>
         </div>
-        <a class="nav-link logout-btn" href="logout.php">Logout</a>
-    </div>
     </div>
 </nav>
