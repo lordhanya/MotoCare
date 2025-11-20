6072bff1c1baef05a0a906b260a25426e8bf3d30
lordhanya <ashifrahman8638471722@gmail.com>
2025-11-20 13:39:34 +0530

    feat: Refactor database connection to use environment variables for configuration

--- db/connection.php
+++ db/connection.php
@@ -1,8 +1,17 @@
- <?php
-$servername = "localhost";
-$username = "root";
-$password = "@ARahman0622";
+<?php
 
-$conn = new PDO("mysql:host=$servername;dbname=autocare_db", $username, $password);
+require __DIR__ . '/../vendor/autoload.php';
 
-?> 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
+
+
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
